7c54acbcf7ee0cd56ec5dbf954c4259aa96af06e
asb-school <asb@andrews.edu>
2013-01-24 13:59:34 -0500

    Initial commit

--- mapTest/ViewController.m
+++ mapTest/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#define METERS_PER_MILE 1609.344
+
 @interface ViewController ()
 
 @end
@@ -25,5 +27,32 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    // Setup zoom location
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = 41.961134;
+    zoomLocation.longitude= -86.357324;
+    
+    // Set view region
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+    
+    // Set the view to the defined location
+    [_theMapView setRegion:viewRegion animated:YES];
+    
+    // Now I shall add an annotation
+    CLLocationCoordinate2D hpac;
+    hpac.latitude = 41.960256;
+    hpac.longitude = -86.357539;
+    
+    NSDictionary *addressDict;
+    
+    MKPlacemark *placemark = [[MKPlacemark alloc] initWithCoordinate: hpac addressDictionary: addressDict];
+
+    [_theMapView addAnnotation: placemark];
+    
+}
+
 
 @end
